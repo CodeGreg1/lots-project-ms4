e5d398b824c7661d5e8ee15175b0b4b98d4af2d4
GregWebDev <code@greggoodrem.co.uk>
2023-02-09 11:36:59 +0000

    Pages and pictures added

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about_us.html', views.about_us, name='about_us'),
+    path('links.html', views.links, name='links'),
+    path('membership.html', views.membership, name='membership'),
+    path('publications.html', views.publications, name='publications'),
+    path('sales.html', views.sales, name='sales'),
+    path('news.html', views.news, name='news'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,33 @@ from django.contrib.staticfiles import storage
 def index(request):
     """ A view to return the index page """
     return render(request, "home/index.html")
+
+
+def about_us(request):
+    """ A view to return the index page """
+    return render(request, "home/about_us.html")
+
+
+def links(request):
+    """ A view to return the links page """
+    return render(request, "home/links.html")
+
+
+def membership(request):
+    """ A view to return the membership page """
+    return render(request, "home/membership.html")
+
+
+def publications(request):
+    """ A view to return the publications page """
+    return render(request, "home/publications.html")
+
+
+def sales(request):
+    """ A view to return the sales page """
+    return render(request, "home/sales.html")
+
+
+def news(request):
+    """ A view to return the news page """
+    return render(request, "home/news.html")
