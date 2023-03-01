669471920df362f99ceed52f4f290de3349cac89
GregWebDev <code@greggoodrem.co.uk>
2023-03-01 11:05:44 +0000

    Stuck on passing Checkout success

--- checkout/models.py
+++ checkout/models.py
@@ -10,7 +10,7 @@ from products.models import Product
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
-    membership_num = models.CharField(max_length=20, null=False, blank=False)
+    membership_num = models.CharField(max_length=20, null=True, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -27,8 +27,8 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
-    # stripe_pid = models.CharField(max_length=254, null=False, blank=False,
-    #   default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False,
+      default='')
 
     def _generate_order_number(self):
         """
@@ -85,6 +85,3 @@ class OrderLineItem(models.Model):
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
-
-
-
--- checkout/urls.py
+++ checkout/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from .webhooks import webhook
 
 from . import views
 
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),  # noqa
-    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),  # noqa
+    path('checkout_success/<order_number>/', views.checkout_success, name='checkout_success'),  # noqa
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),  # 
+    path('wh/', webhook, name='webhook')
 ]
