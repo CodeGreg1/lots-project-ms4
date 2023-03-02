71608ff65994d7e12d5b00554355eeed6850ac64
GregWebDev <code@greggoodrem.co.uk>
2023-03-02 12:11:58 +0000

    Updated Env

--- lots_website/settings.py
+++ lots_website/settings.py
@@ -22,13 +22,7 @@ if os.path.exists("env.py"):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i4viu@33bnw#_!sf1#0nrkup8gz6zya7shjg2)s26r6#ip@u8z'  # noqa
-# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
