683b59434024dcc1ee104a4d83239b35402482be
GregWebDev <code@greggoodrem.co.uk>
2023-03-02 21:36:10 +0000

    Procfile added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lots-project-ms4.wsgi:application
--- lots_website/settings.py
+++ lots_website/settings.py
@@ -187,7 +187,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'testwebgwd@example.com'
+    DEFAULT_FROM_EMAIL = 'testwebgwd@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
