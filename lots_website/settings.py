a4139e942b77a3833d62d630970176530185b81f
GregWebDev <code@greggoodrem.co.uk>
2023-03-06 14:18:11 +0000

    Staticfiles added to end and eu2

--- lots_website/settings.py
+++ lots_website/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
@@ -213,3 +213,5 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
