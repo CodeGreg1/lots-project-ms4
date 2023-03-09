3740901e3070b7f102a8e39a5e38251c89d86f5f
GregWebDev <code@greggoodrem.co.uk>
2023-03-09 12:27:50 +0000

    message changed in email

--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
 
 
 def contact(request):
     if request.method == 'POST':
-        message = request.POST['message']
+        message_received = request.POST['message_received']
         reply_to = request.POST['reply_to']
         from_name = request.POST['from_name']
         contact_number = request.POST['contact_number']
@@ -13,7 +13,7 @@ def contact(request):
         send_mail(
             f'LOTS Contact form filled in by {from_name}',
             f'Name:{from_name} \
-            Message: {message}\
+            Message: {message_received}\
             Number: {contact_number}\
             Email: {reply_to}',
             reply_to,
@@ -21,7 +21,6 @@ def contact(request):
             fail_silently=False,
             auth_user=None,
             auth_password=None,
-            connection=None,
             html_message=None,
         )
         return render(request, 'contact/contact.html', {'from_name': from_name}, )  # noqa
